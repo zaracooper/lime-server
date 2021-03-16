3a0d75776b65fe2a80913ea4b053c358a750285a
zaracooper <zramcooper@gmail.com>
2021-03-16 07:44:59 +0300

    Add token helper

---
+++ helpers/access_token.js
@@ -0,0 +1,25 @@
+import dayjs from 'dayjs';
+
+class AccessToken {
+    value;
+    type;
+    expiresIn;
+    scope;
+    createdAt;
+    lastSaved;
+
+    constructor(contents) {
+        this.value = contents.access_token;
+        this.type = contents.token_type;
+        this.expiresIn = contents.expires_in;
+        this.scope = contents.scope;
+        this.createdAt = contents.created_at;
+        this.lastSaved = contents.last_saved;
+    }
+
+    isValid() {
+        return dayjs(this.lastSaved).add(this.expiresIn, 's').isAfter(dayjs());
+    }
+}
+
+export default AccessToken;
