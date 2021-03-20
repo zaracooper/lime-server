69a5695070681db4b9c3aa0ed6bac105d3c0c2d5
zaracooper <zramcooper@gmail.com>
2021-03-20 19:22:53 +0300

    Change config exports

--- config/index.js
+++ config/index.js
@@ -4,7 +4,16 @@ if (process.env.NODE_ENV == 'development') {
     dotenv.config();
 }
 
-export const sessionDB = {
+const commerceLayer = {
+    domain: process.env.COMMERCE_LAYER_DOMAIN,
+    clientId: process.env.COMMERCE_LAYER_CLIENT_ID,
+    euScope: process.env.COMMERCE_LAYER_EUROPE_SCOPE,
+    usScope: process.env.COMMERCE_LAYER_USA_SCOPE,
+    euWHScope: process.env.COMMERCE_LAYER_EU_WAREHOUSE_SCOPE,
+    usWHScope: process.env.COMMERCE_LAYER_US_WAREHOUSE_SCOPE
+};
+
+const sessionDB = {
     host: process.env.SESSION_DB_HOST,
     user: process.env.SESSION_DB_USER,
     pass: process.env.SESSION_DB_PASS,
@@ -13,11 +22,4 @@ export const sessionDB = {
     secret: process.env.SESSION_DB_SECRET
 };
 
-export const commerceLayer = {
-    domain: process.env.COMMERCE_LAYER_DOMAIN,
-    clientId: process.env.COMMERCE_LAYER_CLIENT_ID,
-    euScope: process.env.COMMERCE_LAYER_EUROPE_SCOPE,
-    usScope: process.env.COMMERCE_LAYER_USA_SCOPE,
-    euWHScope: process.env.COMMERCE_LAYER_EU_WAREHOUSE_SCOPE,
-    usWHScope: process.env.COMMERCE_LAYER_US_WAREHOUSE_SCOPE
-};
+export { commerceLayer, sessionDB };
