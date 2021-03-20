3373c6e2c5bf22bd6c6c8e5fc1702937c43f598c
zaracooper <zramcooper@gmail.com>
2021-03-20 19:24:03 +0300

    Remove unused functions

---
+++ helpers/auth.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { commerceLayer } from '../config/index.js';
+
+function checkAccessToken(req, res, next) {
+    if (req.session.accessToken) {
+        next();
+    } else {
+        res.status(401).send({ message: 'Authentication required to acess this route.' });
+    }
+}
+
+function checkCustomerToken(req, res, next) { }
+
+export { checkAccessToken, checkCustomerToken };
